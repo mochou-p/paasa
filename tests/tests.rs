605a539bdff03e7962383475a6d605a4188ad847
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-12-27 01:25:02 +0900

    initial commit, rust line parsing base

---
+++ tests/tests.rs
@@ -0,0 +1,27 @@
+//
+
+use temp::{tokenise_line, Token};
+
+
+#[test]
+fn test_001() {
+    let line        = "pub fn test::<T>(&mut self, lol: &u8, lmao: Vec<Vec<T>>) { // :D :O";
+    let tokens      = tokenise_line(line);
+    let expectation = {
+        use Token::*;
+
+        vec![
+            /* pub fn test       */ Pub, Fn, FnName,
+            /* ::<T>             */ DoubleColon, GenericStart, Type, GenericEnd,
+            /* (                 */ ParenStart,
+            /* &mut self,        */ Reference, Mut, SmallSelf, Comma,
+            /* lol:  &u8         */ VarName, Colon, Reference, Type, Comma,
+            /* lmao: Vec<Vec<T>> */ VarName, Colon, Type, GenericStart, Type, GenericStart, Type, GenericEnd, GenericEnd,
+            /* )                 */ ParenEnd,
+            /* { // :D :O        */ ScopeStart, Comment
+        ]
+    };
+
+    assert_eq!(tokens, expectation);
+}
+
