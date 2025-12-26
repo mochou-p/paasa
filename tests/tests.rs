2a0aa1406d7f1127bee636801afa94af689b7144
mochou-p <66534926+mochou-p@users.noreply.github.com>
2025-12-27 01:42:26 +0900

    prepare structure for more future langs

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,27 +1,29 @@
 //
 
-use temp::{tokenise_line, Token};
+#[cfg(feature = "rust")]
+mod rust_tests {
+    use temp::rust::{tokenise_line, Token};
 
+    #[test]
+    fn rust_test_001() {
+        let line        = "pub fn test::<T>(&mut self, lol: &u8, lmao: Vec<Vec<T>>) { // :D :O";
+        let tokens      = tokenise_line(line);
+        let expectation = {
+            use Token::*;
 
-#[test]
-fn test_001() {
-    let line        = "pub fn test::<T>(&mut self, lol: &u8, lmao: Vec<Vec<T>>) { // :D :O";
-    let tokens      = tokenise_line(line);
-    let expectation = {
-        use Token::*;
+            vec![
+                /* pub fn test       */ Pub, Fn, FnName,
+                /* ::<T>             */ DoubleColon, GenericStart, Type, GenericEnd,
+                /* (                 */ ParenStart,
+                /* &mut self,        */ Reference, Mut, SmallSelf, Comma,
+                /* lol:  &u8         */ VarName, Colon, Reference, Type, Comma,
+                /* lmao: Vec<Vec<T>> */ VarName, Colon, Type, GenericStart, Type, GenericStart, Type, GenericEnd, GenericEnd,
+                /* )                 */ ParenEnd,
+                /* { // :D :O        */ ScopeStart, Comment
+            ]
+        };
 
-        vec![
-            /* pub fn test       */ Pub, Fn, FnName,
-            /* ::<T>             */ DoubleColon, GenericStart, Type, GenericEnd,
-            /* (                 */ ParenStart,
-            /* &mut self,        */ Reference, Mut, SmallSelf, Comma,
-            /* lol:  &u8         */ VarName, Colon, Reference, Type, Comma,
-            /* lmao: Vec<Vec<T>> */ VarName, Colon, Type, GenericStart, Type, GenericStart, Type, GenericEnd, GenericEnd,
-            /* )                 */ ParenEnd,
-            /* { // :D :O        */ ScopeStart, Comment
-        ]
-    };
-
-    assert_eq!(tokens, expectation);
+        assert_eq!(tokens, expectation);
+    }
 }
 
